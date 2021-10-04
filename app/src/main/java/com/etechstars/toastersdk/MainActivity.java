ae120ca77e1de3ceb32fd1ce79e2f6ca4dfbe643
Harsheo <harsh.harsheo@gmail.com>
2021-10-04 23:55:59 +0530

    Initial Commit

---
+++ app/src/main/java/com/etechstars/toastersdk/MainActivity.java
@@ -0,0 +1,26 @@
+package com.etechstars.toastersdk;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.etechstars.toastersdk.databinding.ActivityMainBinding;
+
+
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mTextView = binding.text;
+    }
+}
